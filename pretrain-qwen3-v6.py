7a4839ec8dee5b9c539cb49bd059d3c27a23f8e1
metastable-void <git@yuka.h09.eu>
2025-06-08 19:48:24 +0900

    v6

---
+++ pretrain-qwen3-v6.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+
+import json
+
+from transformers import Trainer, TrainingArguments
+from transformers import T5Tokenizer, Qwen3ForCausalLM, AutoConfig
+from datasets import load_dataset, DatasetDict, concatenate_datasets
+from transformers import DataCollatorForLanguageModeling
+
+
+ds_train = load_dataset("./datasets/train", split="train")
+ds_valid = load_dataset("./datasets/validation", split="train")
+
+raw_datasets = DatasetDict(
+    {
+        "train": concatenate_datasets([ds_train, ds_valid]),  # .shuffle().select(range(50000)),
+    }
+)
+
+
+context_length = 2048
+tokenizer = T5Tokenizer.from_pretrained("rinna/japanese-gpt-1b")
+
+TOKENS = []
+
+with open('words-old.json', 'r') as file:
+    for token in json.load(file):
+        TOKENS.append(token)
+
+tokenizer.add_tokens(TOKENS)
+
+print(raw_datasets)
+
+def tokenize(element):
+    outputs = tokenizer(
+        element["text"],
+    )
+    input_batch = []
+    n = context_length
+
+    for input_ids in outputs["input_ids"]:
+        for input_id in input_ids:
+            input_batch.append(input_id)
+        input_batch.append(tokenizer.eos_token_id)
+    batch = [input_batch[i:i + n] for i in range(0, len(input_batch), n)]
+    return {"input_ids": [item for item in batch if len(item) == n]}
+
+tokenized_datasets = raw_datasets.map(
+    tokenize, batched=True, remove_columns=raw_datasets["train"].column_names
+)
+
+print(tokenized_datasets)
+
+config = AutoConfig.from_pretrained(
+    "Qwen/Qwen3-0.6B",
+    vocab_size=len(tokenizer),
+    n_ctx=context_length,
+    bos_token_id=tokenizer.bos_token_id,
+    eos_token_id=tokenizer.eos_token_id,
+)
+
+model = Qwen3ForCausalLM(config)
+
+model_size = sum(t.numel() for t in model.parameters())
+print(f"Qwen3 size: {model_size/1000**2:.1f}M parameters")
+
+tokenizer.pad_token = tokenizer.eos_token
+data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+
+args = TrainingArguments(
+    output_dir="qwen3-0.6b-vericava-posts-v6",
+    auto_find_batch_size=True,
+    eval_strategy="no",
+    logging_steps=140,
+    gradient_accumulation_steps=8,
+    num_train_epochs=100,
+    weight_decay=0.1,
+    warmup_steps=300,
+    lr_scheduler_type="cosine",
+    learning_rate=2e-4,
+    save_steps=100,
+    bf16=True,
+    push_to_hub=True,
+)
+
+trainer = Trainer(
+    model=model,
+    tokenizer=tokenizer,
+    args=args,
+    data_collator=data_collator,
+    train_dataset=tokenized_datasets["train"],
+)
+
+trainer.train()
+
+trainer.push_to_hub()
+
