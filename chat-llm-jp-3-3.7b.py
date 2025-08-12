55114b28d70f153fd2ed1e2b8ab6c8d359a556fb
metastable-void <git@yuka.h09.eu>
2025-08-12 22:03:20 +0900

    update

---
+++ chat-llm-jp-3-3.7b.py
@@ -0,0 +1,43 @@
+import torch
+from transformers import pipeline
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+pipe = pipeline(
+    "text-generation", model="vericava/llm-jp-3-vericava-posts-v1", device=device
+)
+
+try:
+    while True:
+        user_input = " ".join(input("> ").strip().split("\n"))
+        if user_input == "":
+            continue
+
+        user_input = user_input if (
+            user_input.endswith("。")
+            or user_input.endswith("?")
+            or user_input.endswith("!")
+            or user_input.endswith("？")
+            or user_input.endswith("！")
+        ) else user_input + "。"
+
+        gen_text = pipe(
+            user_input,
+            num_return_sequences=1,
+            temperature=0.6,
+            top_p=0.95,
+            top_k=20,
+            min_p=0,
+            max_new_tokens=512,
+            do_sample=True,
+            repetition_penalty=2.0,
+        )[0]["generated_text"]
+        gen_text = gen_text[len(user_input):]
+
+        gen_text = gen_text[:gen_text.find("\n")] if "\n" in gen_text else gen_text
+        gen_text = gen_text[:(gen_text.rfind("。") + 1)] if "。" in gen_text else gen_text
+
+        print(gen_text)
+except EOFError:
+    print("\nBye. Exiting.")
+except KeyboardInterrupt:
+    print("\nInterrupted. Bye.")
